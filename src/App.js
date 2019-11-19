e2c8bb0192a6ab5bd6200c675ae055699468b1c1
timqian <timqian92@qq.com>
2019-11-19 19:04:40 +0800

    ajust icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
+import queryString from 'query-string';
+import notie from 'notie';
 import 'codemirror/lib/codemirror.css';
 import 'tui-editor/dist/tui-editor.min.css';
+import 'tui-editor/dist/tui-editor-extScrollSync';
 import { Editor } from '@toast-ui/react-editor'
 import printJS from 'print-js';
 import './components/Dropdown.css';
@@ -16,27 +19,32 @@ import tuiCssForPdf from "./customization/tuiCssForPdf";
 import timqianCssForPdf from "./customization/timqianCssForPdf";
 import timqianCssForEditor from "./customization/timqianCssForEditor";
 
-function App() {
-  const themes = {
-    tui: {
-      name: 'tui',
-      pdfCss: tuiCssForPdf,
-      editorCss: tuiCssForEditor,
-    },
-    github: {
-      name: 'github',
-      pdfCss: githubCssForPdf,
-      editorCss: githubCssForEditor,
-    },
-    timqian: {
-      name: 'timqian',
-      pdfCss: timqianCssForPdf,
-      editorCss: timqianCssForEditor,
-    }
+const themes = {
+  tui: {
+    name: 'tui',
+    pdfCss: tuiCssForPdf,
+    editorCss: tuiCssForEditor,
+  },
+  github: {
+    name: 'github',
+    pdfCss: githubCssForPdf,
+    editorCss: githubCssForEditor,
+  },
+  timqian: {
+    name: 'timqian',
+    pdfCss: timqianCssForPdf,
+    editorCss: timqianCssForEditor,
   }
-  const editorEl = useRef(null);
+}
 
-  const initMdContent = window.localStorage.getItem('mdContent') ? window.localStorage.getItem('mdContent') : initContent;
+function App() {
+  const editorEl = useRef(null);
+  const queryObj = queryString.parse(window.location.search);
+  const queryMdEncoded = queryObj.md;
+  const localMd = window.localStorage.getItem('mdContent');
+  let initMdContent = initContent;
+  if (localMd) initMdContent = localMd;
+  if (queryMdEncoded) initMdContent = decodeURIComponent(queryMdEncoded);
   const [theme, setTheme] = useState(themes.tui.name);
 
   // only run on first render.
@@ -45,7 +53,7 @@ function App() {
       css: themes[theme].editorCss,
       id: themes[theme].name,
     });
-  }, [])
+  }, []);
 
 
   const updateTheme = (newTheme) => {
@@ -57,7 +65,7 @@ function App() {
     });
   }
 
-  const onMdContentChange = (e) => {
+  const onMdContentChange = () => {
     const value = editorEl.current.getInstance().getValue();
     window.localStorage.setItem('mdContent', value);
   }
@@ -91,11 +99,18 @@ function App() {
     });
   }
 
+  const getShareLink = () => {
+    const md = window.localStorage.getItem('mdContent');
+    const url = window.location.origin + '?' + queryString.stringify({md: encodeURIComponent(md)});
+    console.log(url);
+    notie.alert({ text: `Share this editable resume to others with <br/><a href="${url}">this link</a>`, type: 'success', time: 6});
+  }
+
   return (
     <div className="App">
       <header>
         <div className="header-left">
-          <img src="https://i.v2ex.co/e0W134z7.png" className="App-logo" alt="logo" />
+          <a href="/"><img src="https://i.v2ex.co/e0W134z7.png" className="App-logo" alt="logo" /></a>
         </div>
         <div className="header-right"> 
           <div className="dropdown">
@@ -105,18 +120,22 @@ function App() {
               <a href onClick={() => updateTheme(themes.timqian.name)}>timqian.com</a>
               <a href onClick={() => updateTheme(themes.github.name)}>GitHub</a>
               <hr/>
-              <a href><i><small>Customize</small></i></a>
+              <a href="https://github.com/timqian/resumd/issues/1"><i><small>Customize</small></i></a>
             </div>
           </div>
           <div className="dropdown">
-            <a className="header-link" href onClick={() => downloadPDF()}> Download PDF <span style={{fontSize:'10px'}}>▼</span></a>
+            <a className="header-link" href> Download <span style={{fontSize:'10px'}}>▼</span></a>
             <div className="dropdown-content">
               <a href onClick={() => downloadPDF()}>PDF</a>
               <a href onClick={() => downloadMD()}>Markdown</a>
               <a href onClick={() => downloadHTML()}>HTML</a>
             </div>
           </div>
-          <a className="header-link">Get Link</a>
+          <a className="header-link" href onClick={() => getShareLink()}>
+            <span>Get Link </span> 
+            
+            <svg xmlns="http://www.w3.org/2000/svg" width="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-share-2"><circle cx="18" cy="5" r="3"></circle><circle cx="6" cy="12" r="3"></circle><circle cx="18" cy="19" r="3"></circle><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line></svg>
+          </a>
         </div>
       </header>
       <Editor
@@ -126,32 +145,23 @@ function App() {
         height="calc(100vh - 64px)"
         initialEditType="markdown"
         useCommandShortcut={true}
-        onChange={(e)=>onMdContentChange(e)}
+        onChange={() => onMdContentChange()}
         exts={[
-          {
-            name: 'chart',
-            minWidth: 100,
-            maxWidth: 600,
-            minHeight: 100,
-            maxHeight: 300
-          },
           'scrollSync',
-          'colorSyntax',
-          'uml',
-          'mark',
-          'table'
         ]}
       />
 
       <div className="ad-div">
-        <a href="https://www.patreon.com/timqian"><span role="img" aria-label="donate">❤️</span>Donate</a>
+        <a href="https://www.patreon.com/timqian">
+          <svg xmlns="http://www.w3.org/2000/svg" width="15" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>
+          <span> Donate</span>
+        </a>
         &nbsp;
         |
         &nbsp;
         <a href="https://github.com/timqian/resumd/issues">
-          <svg width="15" viewBox="0 0 16 16" version="1.1" aria-hidden="true"><path fill-rule="evenodd" fill="#fff"d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
-          &nbsp;
-          Feedback
+          <svg xmlns="http://www.w3.org/2000/svg" width="15" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
+          <span> Feedback</span>
         </a>
       </div>
     </div>
